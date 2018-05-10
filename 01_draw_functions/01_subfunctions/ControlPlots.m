538d778be67389af39833770993e775093062a87
Philipp Oettershagen <philipp.oettershagen@mavt.ethz.ch>
2018-05-10 15:51:38 +0200

    1) Update logscripts to message names in current pixhawk firmware 2) extend control plot script 3) add subplot_tight for tighter plotting (e.g. used in control plots now)

--- 01_draw_functions/01_subfunctions/ControlPlots.m
+++ 01_draw_functions/01_subfunctions/ControlPlots.m
@@ -23,45 +23,67 @@ yawRef = getsampleusingtime(sysvector('vehicle_attitude_setpoint_0.yaw_body'),..
 yawRef.DataInfo.Interpolation = tsdata.interpolation('zoh');
 
 fig16 = figure(16);
-fig16.Name = 'Attitude and Rate Control';
-% rates plot
-control(1) = subplot(4,1,1);
-hold on;
-plot(sysvector('vehicle_attitude_0.rollspeed').Time, sysvector('vehicle_attitude_0.rollspeed').Data);
-plot(pRef.Time, pRef.Data);
-plot(sysvector('vehicle_attitude_0.pitchspeed').Time, sysvector('vehicle_attitude_0.pitchspeed').Data);
-plot(qRef.Time, qRef.Data);
-plot(sysvector('vehicle_attitude_0.yawspeed').Time, sysvector('vehicle_attitude_0.yawspeed').Data);
-plot(rRef.Time, rRef.Data);
-hold off;
-legend('p','pRef','q','qRef','r','rRef');
-ylabel('Rates [rad/s]')
+fig16.Name = 'Attitude+Airspd+Alt Control';
+nrSubplotSections = 10;
+plotmargins.horiz = 0.05;
+plotmargins.vert = 0.016;
+plotmargins.vert_last = 0.055;
 
-% angles plot
+% Mode and state plot
+axeshandle(1) = subplot_tight(nrSubplotSections,1,1,[plotmargins.vert plotmargins.horiz]);
+hold on;
+stairs(sysvector('commander_state_0.main_state').Time, sysvector('commander_state_0.main_state').Data);
+ylabel('Mode [-]')
+hold off
+
+% Angle plots
 [pitch, roll, yaw] = ...
         QuaternionToEuler(sysvector('vehicle_attitude_0.q_0'), sysvector('vehicle_attitude_0.q_1'),...
         sysvector('vehicle_attitude_0.q_2'), sysvector('vehicle_attitude_0.q_3'));
-    
-control(2) = subplot(4,1,2);
+
+axeshandle(end+1) = subplot_tight(nrSubplotSections,1,2,[plotmargins.vert plotmargins.horiz]);
 hold on;
-plot(roll.Time, roll.Data);
-plot(rollRef.Time, rollRef.Data);
-plot(pitch.Time, pitch.Data);
-plot(pitchRef.Time, pitchRef.Data);
+plot(yaw.Time, rad2deg(yaw.Data));
+plot(yawRef.Time, rad2deg(yawRef.Data));
+hold off;
+legend('Yaw Angle', 'Yaw Angle Ref')
+%ylabel('Attitude [deg]')
+
+axeshandle(end+1) = subplot_tight(nrSubplotSections,1,[3 4],[plotmargins.vert plotmargins.horiz]);
+hold on;
+plot(roll.Time, rad2deg(roll.Data));
+plot(rollRef.Time, rad2deg(rollRef.Data));
+plot(pitch.Time, rad2deg(pitch.Data));
+plot(pitchRef.Time, rad2deg(pitchRef.Data));
 hold off;
 legend('Roll Angle', 'Roll Angle Ref', 'Pitch Angle', 'Pitch Ref');    
 ylabel('Attitude [deg]')
 
-% yaw angle plot
-control(3) = subplot(4,1,3);
+% rates plot
+axeshandle(end+1) = subplot_tight(nrSubplotSections,1,[5 6],[plotmargins.vert plotmargins.horiz]);
 hold on;
-plot(yaw.Time, yaw.Data);
-plot(yawRef.Time, yawRef.Data);
+plot(sysvector('vehicle_attitude_0.rollspeed').Time, rad2deg(sysvector('vehicle_attitude_0.rollspeed').Data));
+plot(pRef.Time, pRef.Data);
+plot(sysvector('vehicle_attitude_0.pitchspeed').Time, rad2deg(sysvector('vehicle_attitude_0.pitchspeed').Data));
+plot(qRef.Time, qRef.Data);
+plot(sysvector('vehicle_attitude_0.yawspeed').Time, rad2deg(sysvector('vehicle_attitude_0.yawspeed').Data));
+plot(rRef.Time, rRef.Data);
 hold off;
-legend('Yaw Angle', 'Yaw Angle Ref')
-ylabel('Attitude [deg]')
+legend('p','pRef','q','qRef','r','rRef');
+ylabel('Rates [deg/s]')
 
-control(4) = subplot(4,1,4);
+axeshandle(end+1) = subplot_tight(nrSubplotSections,1,[7 8],[plotmargins.vert plotmargins.horiz]);
+% airspeed plots
+hold on;
+plot(sysvector('actuator_controls_0.control_0').Time, sysvector('actuator_controls_0.control_0').Data);
+plot(sysvector('actuator_controls_0.control_1').Time, sysvector('actuator_controls_0.control_1').Data);
+plot(sysvector('actuator_controls_0.control_2').Time, sysvector('actuator_controls_0.control_2').Data);
+plot(sysvector('actuator_controls_0.control_3').Time, sysvector('actuator_controls_0.control_3').Data);
+plot(sysvector('actuator_controls_0.control_4').Time, sysvector('actuator_controls_0.control_4').Data);
+legend('u_{ail}','u_{elev}', 'u_{red}', 'u_{throt}', 'u_{flaps}');
+ylabel('Act. outputs []')
+
+axeshandle(end+1) = subplot_tight(nrSubplotSections,1,[9],[plotmargins.vert plotmargins.horiz]);
 % airspeed plots
 hold on;
 plot(sysvector('airspeed_0.true_airspeed_m_s').Time, sysvector('airspeed_0.true_airspeed_m_s').Data);
@@ -69,24 +91,21 @@ plot(sysvector('airspeed_0.indicated_airspeed_m_s').Time, sysvector('airspeed_0.
 plot(sysvector('tecs_status_0.airspeedSp').Time, sysvector('tecs_status_0.airspeedSp').Data);
 % TODO add here v ref nom, v ref min, v ref max
 legend('v_{TAS} [m/s]','v_{IAS} [m/s]', 'v_{IAS} ref[m/s]');
-ylabel('Airspeed [m/s]')
-hold off;
+ylabel('Airsp. [m/s]')
 
-linkaxes(control(:),'x');
-set(control(:),'XGrid','on','YGrid','on','ZGrid','on');
-
-
-fig17 = figure(17);
-fig17.Name = 'Airspeed Control';
 % altitude plot
-hold on;
+axeshandle(end+1) = subplot_tight(nrSubplotSections,1,[10],[plotmargins.vert_last plotmargins.horiz]);
 plot(sysvector('vehicle_global_position_0.alt').Time, sysvector('vehicle_global_position_0.alt').Data);
 plot(sysvector('tecs_status_0.altitudeSp').Time, sysvector('tecs_status_0.altitudeSp').Data);
 plot(sysvector('vehicle_gps_position_0.alt').Time, sysvector('vehicle_gps_position_0.alt').Data*fconv_gpsalt);
 % TODO add here alt min/max and altitude ramp (if that exists)
 legend('Altitude estimate [m]', 'Altitude ref[m]', 'GPS Alt [m]');
 xlabel('Time [s]')
-ylabel('Altitude [m]')
-hold off;
+ylabel('Alt. [m]')
+
+for i=1:length(axeshandle)-1; set(axeshandle(end-i),'XTickLabel',''); end;
+linkaxes(axeshandle(:),'x');
+set(axeshandle(:),'XGrid','on','YGrid','on','ZGrid','on');
+
 end
 
--- 01_draw_functions/01_subfunctions/SensorPlots.m
+++ 01_draw_functions/01_subfunctions/SensorPlots.m
@@ -9,9 +9,9 @@ function SensorPlots(sysvector, topics, fconv_gpsalt)
     fig4.Name = 'Sensor Data';
     raw(1)=subplot(4,1,1);
     hold on;
-    plot(sysvector('sensor_combined_0.magnetometer_ga_0').Time,sysvector('sensor_combined_0.magnetometer_ga_0').Data);
-    plot(sysvector('sensor_combined_0.magnetometer_ga_1').Time,sysvector('sensor_combined_0.magnetometer_ga_1').Data);
-    plot(sysvector('sensor_combined_0.magnetometer_ga_2').Time,sysvector('sensor_combined_0.magnetometer_ga_2').Data);
+    plot(sysvector('sensor_mag_0.x').Time,sysvector('sensor_mag_0.x').Data);
+    plot(sysvector('sensor_mag_0.y').Time,sysvector('sensor_mag_0.y').Data);
+    plot(sysvector('sensor_mag_0.z').Time,sysvector('sensor_mag_0.z').Data);
     hold off
     title('Magnetometers [Gauss]');
     legend('x','y','z');
@@ -33,7 +33,7 @@ function SensorPlots(sysvector, topics, fconv_gpsalt)
     legend('x','y','z');
     raw(4)=subplot(4,1,4);
     hold on;
-    plot(sysvector('sensor_combined_0.baro_alt_meter').Time,sysvector('sensor_combined_0.baro_alt_meter').Data);
+    plot(sysvector('vehicle_air_data.baro_alt_meter').Time,sysvector('vehicle_air_data.baro_alt_meter').Data);
     if topics.vehicle_gps_position.logged
         plot(sysvector('vehicle_gps_position_0.alt').Time,double(sysvector('vehicle_gps_position_0.alt').Data)*fconv_gpsalt,'color','red');
         legend('Barometric Altitude [m]','GPS Altitude [m]');
