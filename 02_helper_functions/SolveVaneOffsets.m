b0e7db699bc780409cf0a4f6d58a12bab5b1ee64
Thomas Stastny <thomas.stastny@mavt.ethz.ch>
2019-05-16 00:18:00 +0200

    analysis tools: add script for airflow vane mounting bias calibration

---
+++ 02_helper_functions/SolveVaneOffsets.m
@@ -0,0 +1,132 @@
+% / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+% Airspeed scale solver / / / / / / / / / / / / / / / / / / / / / / / / / /
+function [xopt, opt_info, mean_gsp_err, std_gsp_err] = SolveVaneOffsets(sysvector, paramvector, tspan, x0, lb, ub)
+
+
+% calibrated airflow angle measurements
+aoa_meas = timeseries(deg2rad(1e-7 * paramvector.cal_hall_rev.Data(1) * ...
+        (paramvector.cal_hall_p0.Data(1) + ...
+        paramvector.cal_hall_p1.Data(1) .* sysvector.sensor_hall_0.mag_T.Data + ...
+        paramvector.cal_hall_p2.Data(1) .* sysvector.sensor_hall_0.mag_T.Data .* sysvector.sensor_hall_0.mag_T.Data + ...
+        paramvector.cal_hall_p3.Data(1) .* sysvector.sensor_hall_0.mag_T.Data .* sysvector.sensor_hall_0.mag_T.Data .* sysvector.sensor_hall_0.mag_T.Data)),...
+        sysvector.sensor_hall_0.mag_T.Time);
+slip_meas = timeseries(deg2rad(1e-7 * paramvector.cal_hall_01_rev.Data(1) * ...
+        (paramvector.cal_hall_01_p0.Data(1) + ...
+        paramvector.cal_hall_01_p1.Data(1) .* sysvector.sensor_hall_01_0.mag_T.Data + ...
+        paramvector.cal_hall_01_p2.Data(1) .* sysvector.sensor_hall_01_0.mag_T.Data .* sysvector.sensor_hall_01_0.mag_T.Data + ...
+        paramvector.cal_hall_01_p3.Data(1) .* sysvector.sensor_hall_01_0.mag_T.Data .* sysvector.sensor_hall_01_0.mag_T.Data .* sysvector.sensor_hall_01_0.mag_T.Data)),...
+        sysvector.sensor_hall_01_0.mag_T.Time);
+
+% synchronise the data
+dt_rs = 0.05;
+time_resampled = tspan(1):dt_rs:tspan(2);
+len_t = length(time_resampled);
+
+% resample inputs / outputs
+aoa_meas = resample(aoa_meas, time_resampled);
+slip_meas = resample(slip_meas, time_resampled);
+vel_n = resample(sysvector.vehicle_gps_position_0.vel_n_m_s, time_resampled);
+vel_e = resample(sysvector.vehicle_gps_position_0.vel_e_m_s, time_resampled);
+vel_d = resample(sysvector.vehicle_gps_position_0.vel_d_m_s, time_resampled);
+airspeed = resample(sysvector.airspeed_0.true_airspeed_m_s, time_resampled);
+q_0 = resample(sysvector.vehicle_attitude_0.q_0, time_resampled);
+q_1 = resample(sysvector.vehicle_attitude_0.q_1, time_resampled);
+q_2 = resample(sysvector.vehicle_attitude_0.q_2, time_resampled);
+q_3 = resample(sysvector.vehicle_attitude_0.q_3, time_resampled);
+
+% construct body to inertial transform
+Hi2b = quat2dcm([q_0.Data, q_1.Data, q_2.Data, q_3.Data]);
+Hb2i_rows = zeros(len_t, 9);
+for i = 1:len_t
+    htemp = Hi2b(:,:,i)';
+    Hb2i_rows(i,:) =  [htemp(1,:), htemp(2,:), htemp(3,:)];
+end
+
+% solve nonlinear least squares
+[xopt,resnorm,residual,exitflag,output] = ...
+    lsqnonlin(@(x)SolveVaneMountingBias(x, vel_n.Data, vel_e.Data, vel_d.Data, ...
+    aoa_meas.Data, slip_meas.Data, airspeed.Data, Hb2i_rows, len_t), x0, lb, ub);
+
+opt_info.resnorm = resnorm;
+opt_info.residual = residual;
+opt_info.exitflag = exitflag;
+opt_info.output = output;
+
+% calculate outputs
+[~,out] = SolveVaneMountingBias(xopt, vel_n.Data, vel_e.Data, vel_d.Data, ...
+    aoa_meas.Data, slip_meas.Data, airspeed.Data, Hb2i_rows, len_t);
+
+% mean / std of ground speed errors
+gsp_err = [vel_n.Data; vel_e.Data; vel_d.Data] - [out(:,1); out(:,2); out(:,3)];
+mean_gsp_err = mean(gsp_err);
+std_gsp_err = std(gsp_err);
+
+% reconstruct wind velocity with mounting biases included
+vel_wind = [vel_n.Data, vel_e.Data, vel_d.Data] - out(:,4:6);
+
+%% / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+% Output function / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+
+figure('color','w','name','Airflow Vane Mounting Bias Solution');
+
+lines_ = lines(7);
+plot_opacity = 0.5;
+
+result_plots(1) = subplot(3,1,1); hold on; grid on; box on;
+plot(time_resampled, rad2deg(aoa_meas.Data) - xopt(1), 'color', lines_(1,:));
+plot(time_resampled, rad2deg(slip_meas.Data) - xopt(2), 'color', lines_(2,:));
+legend('\alpha + b_\alpha', '\beta + b_\beta');
+ylabel('Airflow Angles [deg]');
+
+result_plots(2) = subplot(3,1,2); hold on; grid on; box on;
+plot(time_resampled, vel_wind(:,1), '-.', 'color', lines_(1,:) * plot_opacity + ones(1,3) * (1 - plot_opacity));
+plot(time_resampled, vel_wind(:,2), '-.', 'color', lines_(2,:) * plot_opacity + ones(1,3) * (1 - plot_opacity));
+plot(time_resampled, vel_wind(:,3), '-.', 'color', lines_(3,:) * plot_opacity + ones(1,3) * (1 - plot_opacity));
+plot(time_resampled([1 end]), xopt(3) * ones(1,2), 'color', lines_(1,:));
+plot(time_resampled([1 end]), xopt(4) * ones(1,2), 'color', lines_(2,:));
+plot(time_resampled([1 end]), zeros(1,2), 'linewidth', 2, 'color', lines_(3,:));
+legend('w_n recnstr.', 'w_e recnstr.', 'w_d recnstr.', 'w_n fit', 'w_e fit', 'w_d = ZERO');
+ylabel('Wind Components [m/s]');
+
+result_plots(3) = subplot(3,1,3); hold on; grid on; box on;
+plot(time_resampled, sqrt(vel_n.Data.^2 + vel_e.Data.^2 + vel_d.Data.^2), '-.', 'color', lines_(1,:) * plot_opacity + ones(1,3) * (1 - plot_opacity));
+plot(time_resampled, sqrt(vel_wind(:,1).^2+vel_wind(:,2).^2), '-.', 'color', lines_(2,:) * plot_opacity + ones(1,3) * (1 - plot_opacity));
+plot(time_resampled, airspeed.Data, '-.', 'color', lines_(3,:) * plot_opacity + ones(1,3) * (1 - plot_opacity));
+plot(time_resampled, sqrt(out(:,1).^2 + out(:,2).^2 + out(:,3).^2), 'color', lines_(1,:));
+plot(time_resampled([1 end]), norm(xopt(3) + xopt(4)) * ones(1,2), 'color', lines_(2,:));
+ylabel('Speeds [m/s]');
+legend('3D gnd. sp.', '2D wind sp.', 'u (x-body) airsp.');
+
+xlabel('Time [s]');
+linkaxes(result_plots(:),'x');
+xlim(result_plots(:), time_resampled([1 end]));
+
+% / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+% Output function / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+function [f, out] = SolveVaneMountingBias(x, vel_n_data, vel_e_data, vel_d_data, ...
+    aoa_meas_data, slip_meas_data, airspeed_data, Hb2i_rows, len_t)
+
+% current solution
+b_aoa = x(1);
+b_slip = x(2);
+wn = x(3);
+we = x(4);
+
+% reconstruct airspeed vector in inertial frame
+vb_u = [ones(len_t,1), tan(slip_meas_data - deg2rad(b_slip)), tan(aoa_meas_data - deg2rad(b_aoa))];
+vel_airsp = airspeed_data .* [ ...
+    sum(Hb2i_rows(:,1:3) .* vb_u, 2), ...
+    sum(Hb2i_rows(:,4:6) .* vb_u, 2), ...
+    sum(Hb2i_rows(:,7:9) .* vb_u, 2)];
+
+% reconstruct inertial velocity with zero down wind assumption
+vel_n = vel_airsp(:,1) + wn;
+vel_e = vel_airsp(:,2) + we;
+vel_d = vel_airsp(:,3);
+norm_vel_err = sqrt((vel_n_data - vel_n).^2 + (vel_e_data - vel_e).^2  + (vel_d_data - vel_d).^2);
+
+% objective
+f = [norm_vel_err; slip_meas_data - deg2rad(b_slip)];
+
+% outputs
+out = [vel_n, vel_e, vel_d, vel_airsp];
---
+++ 08_analysis_tools/CalibrateVaneOffsets.m
@@ -0,0 +1,119 @@
+% / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+% Calibrate Airflow Vane Mounting Offsets
+% / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+
+% !!-- This script must be run *after running the logconv.m script. --!!
+
+% NOTE: airspeed scale, tube pressure drop, and vane position mapping
+%       calibrations are assumed completed before running this script.
+
+% Nonlinear least squares solution:
+% min_x ||f(x,u)||
+% subject to
+% w_d = 0, mean slip = 0
+% where 
+% y = f(x,u)
+% y_ref = [vI_n, vI_e, vI_d, slip]
+% x = [b_aoa, b_slip, w_n, w_e]
+% u = [u, aoa, slip, phi, theta, psi]
+
+%% / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+% Calibration data should satisfy the following criteria:
+% - in air at nominal airspeed
+% - constant altitude
+% - executing "figure-8" with minimal bank for at least 2 cycles (hint: use
+%   stabilized modes for collecting this data) -- note both left and right
+%   turns are needed to make the assumption that average slip in nominal
+%   conditions is = zero.
+% - wind speeds are assumed to be constant within the seleted data
+% - no vertical wind is present (assumed zero)
+% NOTE: slip will be assumed = zero in nominal stabilized conditions, this
+% may not be true in reality, but with wind, the mounting offset is
+% otherwise unobservable.
+
+% start and end times (modify these)
+t_st_cal = 2115;
+t_ed_cal = 2160;
+
+clc;
+if (topics.airspeed.logged && ...
+        topics.vehicle_gps_position.logged && ...
+        topics.vehicle_attitude.logged && ...
+        topics.sensor_hall.logged && ...
+        topics.sensor_hall_01.logged)
+    if ~(params.cal_hall_rev.logged && ...
+            params.cal_hall_p0.logged && ...
+            params.cal_hall_p1.logged && ...
+            params.cal_hall_p2.logged && ...
+            params.cal_hall_p3.logged && ...
+            params.cal_hall_01_rev.logged && ...
+            params.cal_hall_01_p0.logged && ...
+            params.cal_hall_01_p1.logged && ...
+            params.cal_hall_01_p2.logged && ...
+            params.cal_hall_01_p3.logged)
+        % if hall calibration params were not present in the logs, they
+        % must be defined here to execute the mounting offset calibration.
+        
+        % aoa
+        paramvector.cal_hall_rev = timeseries(-1, 0);
+        paramvector.cal_hall_p0 = timeseries(-14694678, 0);
+        paramvector.cal_hall_p1 = timeseries(-21239312, 0);
+        paramvector.cal_hall_p2 = timeseries(51980, 0);
+        paramvector.cal_hall_p3 = timeseries(-2845, 0);
+        % slip
+        paramvector.cal_hall_01_rev = timeseries(1, 0);
+        paramvector.cal_hall_01_p0 = timeseries(2696759, 0);
+        paramvector.cal_hall_01_p1 = timeseries(-25891024, 0);
+        paramvector.cal_hall_01_p2 = timeseries(53064, 0);
+        paramvector.cal_hall_01_p3 = timeseries(-7467, 0);
+    end
+    
+        [mean_states, tspan] = VaneMountingCalibrationData(sysvector, topics, paramvector, [t_st_cal, t_ed_cal]);
+    disp(['Mean wind north = ', num2str(mean_states(1)), ' m/s']);
+    disp(['Mean wind east = ', num2str(mean_states(2)), ' m/s']);
+    disp(['Mean sideslip = ', num2str(mean_states(3)), ' deg']);
+else
+    disp('ERROR: logged topics/params are not sufficient for mounting offset calibration.');
+end
+
+%% / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+% Solve nonlinear least squares regression for sensor calibration
+
+% initial guesses
+b_aoa0 = 0;                 % aoa mounting bias [deg]
+b_slip0 = mean_states(3);	% slip mounting bias [deg]
+wn0 = mean_states(1);       % wind speed north [m/s]
+we0 = mean_states(2);       % wind speed east [m/s]
+x0 = [b_aoa0; b_slip0; wn0; we0];
+
+% bounds
+lb = [-10; -10; -20; -20];
+ub = [10; 10; 20; 20];
+
+clc;
+if (topics.airspeed.logged && ...
+        topics.vehicle_gps_position.logged && ...
+        topics.vehicle_attitude.logged && ...
+        topics.sensor_hall.logged && ...
+        topics.sensor_hall_01.logged)
+    
+    % solve
+    [xopt, opt_info, mean_gsp_err, std_gsp_err] = ...
+        SolveVaneOffsets(sysvector, paramvector, tspan, x0, lb, ub);
+    
+    disp(['b_aoa = ',num2str(xopt(1)),' deg']);
+    disp(['b_slip = ',num2str(xopt(2)),' deg']);
+    disp(['wn = ',num2str(xopt(3)),' m/s']);
+    disp(['we = ',num2str(xopt(4)),' m/s']);
+    bounds_exceeded = xopt >= ub | xopt <= lb;
+    if sum(bounds_exceeded)>0
+        % bounds exceeded 
+        disp(['WARNING: optimization bounds exceeded: [',int2str(bounds_exceeded'),']']);
+    end
+    disp(['Mean of gsp. vel. errors: ',num2str(mean_gsp_err),' m/s']);
+    disp(['Standard deviation of gsp. vel. errors: ',num2str(std_gsp_err),' m/s']);
+    disp(['Set CAL_AOA_OFF = ',num2str(xopt(1))]);
+    disp(['Set CAL_SLIP_OFF = ',num2str(xopt(2))]);
+else
+    disp('ERROR: logged topics/params are not sufficient for mounting offset calibration.');
+end
