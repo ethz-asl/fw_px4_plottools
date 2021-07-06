e00fe356f4ebe2ae4d590f9c62c1597b69b525b2
Florian Achermann <florian.achermann@mavt.ethz.ch>
2021-07-06 09:37:56 +0200

    Fix change in vehicle_global_position

--- 02_helper_functions/PrintFlightMetricsSummary.m
+++ 02_helper_functions/PrintFlightMetricsSummary.m
@@ -11,16 +11,20 @@ if (topics.airspeed.logged)
     else
         tLaunch = sysvector.airspeed_0.true_airspeed_m_s.Time(iLaunchAir);
         tLand = sysvector.airspeed_0.true_airspeed_m_s.Time(iLandAir);
-        iLaunchGnd = find(sysvector.vehicle_global_position_0.vel_n.Time > tLaunch,1,'first');
-        iLandGnd = find(sysvector.vehicle_global_position_0.vel_n.Time > tLand,1,'first');
 
         FlightTime = tLand-tLaunch;
         vAirMean = mean(sysvector.airspeed_0.true_airspeed_m_s.Data(iLaunchAir:iLandAir));
-        vGnd = sqrt(sysvector.vehicle_global_position_0.vel_n.Data.^2+sysvector.vehicle_global_position_0.vel_e.Data.^2);
-        vGndMean = mean(vGnd(iLaunchGnd:iLandGnd));
+        if (topics.vehicle_local_position.logged)
+            iLaunchGnd = find(sysvector.vehicle_local_position_0.vx.Time > tLaunch,1,'first');
+            iLandGnd = find(sysvector.vehicle_local_position_0.vx.Time > tLand,1,'first');
+            vGnd = sqrt(sysvector.vehicle_local_position_0.vx.Data.^2+sysvector.vehicle_local_position_0.vy.Data.^2+sysvector.vehicle_local_position_0.vz.Data.^2);
+            vGndMean = mean(vGnd(iLaunchGnd:iLandGnd));
+        else
+           vGndMean = NaN;
+        end
         FlightDistanceAir = vAirMean * FlightTime;
         FlightDistanceGnd = vGndMean * FlightTime;
-        str = sprintf('***********************\n*** Flight Summary: ***\n***********************\ntLaunch: %.2fs tLand:%.2fs tFlight: %.2fs (%s dd:hh:mm:ss) dist_air: %.2fkm dist_gnd: %.2fkm\n', tLaunch, tLand, FlightTime, datestr(FlightTime/86400.0, 'DD:HH:MM:SS'),FlightDistanceAir/1000.0,FlightDistanceGnd/1000.0);
+        str = sprintf('***********************\n*** Flight Summary: ***\n***********************\ntLaunch: %.2f s tLand:%.2f s tFlight: %.2f s (%s dd:hh:mm:ss) dist_air: %.2f km dist_gnd: %.2f km\n', tLaunch, tLand, FlightTime, datestr(FlightTime/86400.0, 'DD:HH:MM:SS'),FlightDistanceAir/1000.0,FlightDistanceGnd/1000.0);
         disp(str);
     end
 
