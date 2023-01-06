a1446ef66eb5a4e8ec337bd66ac842fa628c4f2a
Florian Achermann <florian.achermann@mavt.ethz.ch>
2023-01-06 13:10:25 +0100

    Plot all mag measurements in the MagVsThrustPlots

--- 01_draw_functions/01_subfunctions/MagVsThrustPlots.m
+++ 01_draw_functions/01_subfunctions/MagVsThrustPlots.m
@@ -1,23 +1,41 @@
 function MagVsThrustPlots(sysvector, topics)
 % Plot the mag norm vs thrust
 
-if ~topics.sensor_mag.logged || ~topics.actuator_controls_0.logged
+if ~topics.sensor_mag.logged
     return
 end
 
 fig1 = figure();
 fig1.Name = 'Magnetic Norm vs Thrust';
-
-mag_norm = sqrt(sysvector.sensor_mag_0.x.Data .* sysvector.sensor_mag_0.x.Data +...
-    sysvector.sensor_mag_0.y.Data .* sysvector.sensor_mag_0.y.Data +...
-    sysvector.sensor_mag_0.z.Data .* sysvector.sensor_mag_0.z.Data);
-    
 hold on;
-plot(sysvector.sensor_mag_0.x.Time, mag_norm);
-plot(sysvector.actuator_controls_0_0.control_3.Time, sysvector.actuator_controls_0_0.control_3.Data);
+
+legend_entries = {};
+for i = 0:10
+    fieldname = 'sensor_mag_' + string(i);
+    if isfield(sysvector, fieldname)
+        mag_data = getfield(sysvector, fieldname);
+        mag_norm = sqrt(mag_data.x.Data .* mag_data.x.Data +...
+            mag_data.y.Data .* mag_data.y.Data +...
+            mag_data.z.Data .* mag_data.z.Data);
+
+        plot(mag_data.x.Time, mag_norm);
+        legend_entries = [legend_entries; 'Mag Norm ID ' + string(i)];
+    end
+
+end
+
+if topics.actuator_controls_0.logged
+    plot(sysvector.actuator_controls_0_0.control_3.Time, sysvector.actuator_controls_0_0.control_3.Data);
+    legend_entries = [legend_entries; 'Thrust'];
+end
+
+if topics.battery_status.logged
+    plot(sysvector.battery_status_0.current_filtered_a.Time, sysvector.battery_status_0.current_filtered_a.Data);
+    legend_entries = [legend_entries; 'Filtered Current [A]'];
+end
 hold off;
 
-legend('Magnetic Norm', 'Thrust');
+legend(legend_entries);
 set(gca, 'XGrid','on','YGrid','on','ZGrid','on');
 dcm_obj = datacursormode(fig1);
 set(dcm_obj,'UpdateFcn',@HighPrecisionTooltipCallback);
